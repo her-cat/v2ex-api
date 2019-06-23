1b95492490272e1abe85b6cf93ad30d2960244a2
her-cat <hxhsoft@foxmail.com>
2019-06-23 11:39:40 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the her-cat/v2ex-api.
+ *
+ * (c) her-cat <hxhsoft@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\V2exApi;
 
 /**
